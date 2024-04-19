e490d64607bcd26b230699aa224ae0ccdba85fa9
Your Name <you@example.com>
2024-04-19 21:23:19 +0800

    Добавлена проверка на корректность ввода имени и номера

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -13,10 +13,19 @@ public class PhoneBook {
             if (choice == 1) {
                 System.out.println("Введите имя:");
                 String name = scanner.next();
-                System.out.println("Введите номер телефона:");
-                String phoneNumber = scanner.next();
+                if (name.matches("[а-яА-Яa-zA-Z]+")) {
+                    name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+                    System.out.println("Введите номер телефона:");
+                    String phoneNumber = scanner.next();
 
-                phoneBook.computeIfAbsent(name, k -> new HashSet<>()).add(phoneNumber);
+                    if (phoneNumber.matches("[0-9]+")) {
+                        phoneBook.computeIfAbsent(name, k -> new HashSet<>()).add(phoneNumber);
+                    } else {
+                        System.out.println("Некорректный номер телефона. Пожалуйста, введите номер, состоящий только из цифр.");
+                    }
+                } else {
+                    System.out.println("Некорректное имя. Пожалуйста, введите имя, состоящее только из букв.");
+                }
             } else if (choice == 2) {
                 phoneBook.entrySet().stream()
                         .sorted((entry1, entry2) -> Integer.compare(entry2.getValue().size(), entry1.getValue().size()))
